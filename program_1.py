d9c4b1f53271878afdfdb56ff4d384498f545d54
javangraber-design <javangraber@gmail.com>
2026-03-24 15:33:44 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,13 +1,33 @@
+# Programmer: Javan Graber
+# Date: 3/24/2026
 # Program #1: Item Counter
-# Assume a file containing a series of names (as strings) is named names.txt 
+# Assume a file containing a series of names (as strings) is named names.txt
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
 
 def count_file_lines():
     ######################
-    # Add your code here #
+
+    # Create the original constant for the count
+    count = 0
+    try:
+        # Open the file
+        with open('names.txt', 'r') as count_file:
+
+            # Create a for loop that counts every line and increases the original count
+            for line in count_file:
+                count += 1
+
+    # Prepare for a IOError
+    except IOError:
+
+        # Print an error message
+        print("The file could not be found")
+
+    # Print the count
+    print(f"There are {count} names in the names.txt file")
     ######################
-    print('In the count_file_lines function')
+
 
 
 
